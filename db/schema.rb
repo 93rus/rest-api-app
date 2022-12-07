fa59f3d627dc3621e12bd266c37851684caae27e
Andrey Baranov <andrey.baranov@balace-pl.ru>
2022-12-08 00:17:09 +0300

    fix db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_07_210741) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_211456) do
   create_table "posts", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "post"
-    t.string "likes"
+    t.integer "likes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
